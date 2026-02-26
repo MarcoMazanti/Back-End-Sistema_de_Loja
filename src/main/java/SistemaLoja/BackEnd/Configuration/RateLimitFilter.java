116dcdf0fec58c9ea1b4dc2203905bd1004c43be
Marco Aurélio <marcauremaza@gmail.com>
2026-02-26 20:22:16 -0300

    Testes de Ajustes de Variável e Desativação do Swagger

--- src/main/java/SistemaLoja/BackEnd/Configuration/RateLimitFilter.java
+++ src/main/java/SistemaLoja/BackEnd/Configuration/RateLimitFilter.java
@@ -21,9 +21,9 @@ public class RateLimitFilter extends OncePerRequestFilter {
     private final Map<String, Bucket> buckets = new ConcurrentHashMap<>();
 
     private Bucket createNewBucket() {
-        // 40 requisições permitidas a cada 1 minuto
-        Refill refill = Refill.intervally(40, Duration.ofMinutes(1));
-        Bandwidth limit = Bandwidth.classic(40, refill);
+        // 50 requisições permitidas a cada 1 minuto
+        Refill refill = Refill.intervally(50, Duration.ofMinutes(1));
+        Bandwidth limit = Bandwidth.classic(50, refill);
         return Bucket.builder().addLimit(limit).build();
     }
 
